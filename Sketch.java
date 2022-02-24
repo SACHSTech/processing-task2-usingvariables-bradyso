9d438fb98de0b3f69612405f6cadf848a03f3566
Brady So <99352833+bradyso@users.noreply.github.com>
2022-02-24 17:10:43 +0000

    5.3 Processing Task 2 - Using Variables

--- Sketch.java
+++ Sketch.java
@@ -1,7 +1,10 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+	/**
+  * Description: A program Sketch.java that draws a house with windows and trees. But it will now be able to resize.
+  * Author: Brady So
+  */
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -16,21 +19,51 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    // Background to Sky Blue
+    background(51, 153, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+ 
+    // Drawing the house (square)
+    // Divide original number by 1000 for width, 500 for height
+    fill(153, 0, 0);
+    rect((float) (width * .17), (float) (height * .27), (float) (width * 0.22), (float) (height * .4));
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Drawing the door (rectangle)
+    fill(153, 102, 0);
+    rect((float) (width * .252), (float) (height * .46), (float) (width * .05), (float) (height * .14));
+
+    // Drawing windows (squares)
+    // divide original number by 1000 for width, 500 for height
+    fill(204, 204, 204);
+    rect((float) (width * .32), (float) (height * .314), (float) (width * .04), (float) (height * .08));
+    rect((float) (width * .195), (float) (height * .314), (float) (width * .04), (float) (height * .08));
+    rect((float) (width * .195), (float) (height * .46), (float) (width * .03), (float) (height * .06));
+    rect((float) (width * .325), (float) (height * .46), (float) (width * .03), (float) (height * .06));
+    
+    // Drawing grass (foreground)
+    // Divide original number by 1000 for width, 500 for height
+    fill(0, 204, 0); 
+    rect((float) (width * 0), (float) (height * .56), width, height);
+
+    // Drawing the roof (triangle)
+    fill(102, 51, 0);
+    // Divide original number by 1000 for width, 500 for height
+    triangle((float) (width * .272), (float) (height * .09), (float) (width * .407), (float) (height * .29), (float) (width * .157) , (float) (height * .29));
+
+    // Drawing the trees (Circles & rectangles)
+    // Divide original number by 1000 for width, 500 for height
+    fill(51, 0 ,0);
+    rect((float)(width * .025), (float)(height * .2), (float)(width * .05), (float)(height * .36));
+    fill(0, 153, 0);
+    ellipse((float) (width * .05), (float) (height * .2), (float) (width * .09), (float) (height * .1));
+    fill(51, 0 ,0);
+    rect((float) (width * .485), (float) (height * .2), (float) (width * .05), (float) (height * .36));
+    fill(0, 153, 0);
+    ellipse((float) (width * .51), (float) (height * .2), (float) (width * .09), (float) (height * .1));
   }
-  
-  // define other methods down here.
 }
